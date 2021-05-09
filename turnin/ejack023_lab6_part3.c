5e6eab9ad9fe74ec99480bd194555aa5fae03e43
Ethan Jackson <ejack023@wch136-24.cs.ucr.edu>
2021-05-08 19:56:56 -0700

    complete part 3

--- turnin/ejack023_lab6_part3.c
+++ turnin/ejack023_lab6_part3.c
@@ -1,11 +1,11 @@
 /*	Author: ejack023
- *	Lab Section: 023
- *	Assignment: Lab #6  Exercise #3
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- *
- *	Demo Link: https://www.youtube.com/watch?v=l4wRx0oOEqY&ab_channel=EthanJackson
+ *	 *	Lab Section: 023
+ *	  *	Assignment: Lab #8  Exercise #1
+ *	   *
+ *	    *	I acknowledge all content contained herein, excluding template or example
+ *	     *	code, is my own original work.
+ *	      *
+ *	       *	Demo Link: https://www.youtube.com/watch?v=kaHBFykr-VQ&ab_channel=EthanJackson
  *	        */
 
 #include <avr/io.h>
@@ -48,78 +48,104 @@ ISR(TIMER1_COMPA_vect) {
 }
 void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;	
+	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-enum SM_STATES {SM_start, SM_init, SM_wait, SM_inc, SM_dec, SM_reset} SM_STATE;
+void set_PWM(double frequency) {
+	static double current_frequency;
+	
+	if (frequency != current_frequency) {
+		if(!frequency) {TCCR3B &= 0x08;}
+		else {TCCR3B |= 0x03;}
+			
+		if (frequency < 0.954) {OCR3A = 0xFFFF;}
+			
+		else if (frequency > 31250) {OCR3A = 0x0000;}
+			
+		else {OCR3A = (short)(8000000 / (128*frequency)) - 1;}
+			
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+unsigned char btn = 0x00;
 unsigned char prevInput = 0x00;
-volatile unsigned char input = 0x00;
-unsigned char count = 0;
-
+unsigned char seqNum = 0;
+unsigned char duration = 0;
+unsigned char on = 0;
+double notes[6] = {261.63, 293.66, 349.23, 392.00, 440.00, 523.25}; // C, D, F, G, A, C
+unsigned char sequence[13] = {1, 0, 2, 3, 5, 4, 3, 5, 4, 2, 3, 1, 0}; 
+unsigned char durations[13] = {2, 2, 2, 1, 1, 2, 1, 1, 2, 2, 2, 2, 2}; 
+enum SM1_STATES {SM1_START, SM1_WAIT, SM1_PLAY} SM1_STATE;
 void Tick_Fct() {
-	input = ~PINA & 0x03;
-	switch(SM_STATE) {
-		case SM_start:
-			SM_STATE = SM_init;
+	btn = ~PINA;
+	switch(SM1_STATE) {
+		case SM1_START:
+			SM1_STATE = SM1_WAIT;
 			break;
-		case SM_init:
-			SM_STATE = SM_wait;
+		case SM1_WAIT:
+			if (btn == 0x01 && !prevInput) {
+				PWM_on(); 
+				duration = (durations[seqNum]);
+				SM1_STATE = SM1_PLAY; 
+			}
 			break;
-		case SM_wait:
-			if (input & 0x02 && input & 0x01) SM_STATE = SM_reset;
-			else if (input & 0x01 && !prevInput) {SM_STATE = SM_inc; count = 0;}
-			else if (input & 0x02 && !prevInput) {SM_STATE = SM_dec; count = 0;}
-			break;
-		case SM_inc:
-			if (input & 0x02 && input & 0x01) SM_STATE = SM_reset;
-			else if (!(input & 0x01)) SM_STATE = SM_wait;
-			break;
-		case SM_dec:
-			if (input & 0x02 && input & 0x01) SM_STATE = SM_reset;
-			else if (!(input & 0x02)) SM_STATE = SM_wait;
-			break;
-		case SM_reset:
-			if (!input) SM_STATE = SM_wait;
+		case SM1_PLAY:
+			if (seqNum > 13) {
+				PWM_off(); 
+				seqNum = 0; 
+				SM1_STATE= SM1_WAIT;
+			}
 			break;
 		default:
-			SM_STATE = SM_wait;
-			break; 
+			SM1_STATE = SM1_WAIT;
+			break;
 	}
-	switch(SM_STATE) {
-		case SM_start:
+	switch(SM1_STATE) {
+		case SM1_START:
 			break;
-		case SM_init:
-			PORTB = 7;
+		case SM1_WAIT:
 			break;
-		case SM_wait:
-			break;
-		case SM_inc:
-			if (PORTB < 9 && !(count%10)) ++PORTB;
-			++count;
-			break;
-		case SM_dec:
-			if (PORTB > 0 && !(count%10)) --PORTB;
-			++count;
-			break;
-		case SM_reset:
-			PORTB = 0;
+		case SM1_PLAY:
+			set_PWM(notes[sequence[seqNum]]);
+			--duration;
+			if (duration <= 0) {
+				duration = (durations[++seqNum]);
+			}
 			break;
 		default:
 			break;
 	}
-	prevInput = input;
-}
+	prevInput = btn;
+};
+
 
 int main(void)
 {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	TimerSet(100);
+	DDRA = 0X00; PORTA = 0XFF;
+	DDRB = 0x40; PORTB = 0x00;
+	PWM_off();
+	TimerSet(250);
 	TimerOn();
-	SM_STATE = SM_start;
+	SM1_STATE = SM1_START;
 	while(1) {
 		Tick_Fct();
 		while(!TimerFlag);
 		TimerFlag = 0;
 	}
 }
+
